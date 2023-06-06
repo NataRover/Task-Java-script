7ba66b45bdca5a635226eafe096d93892d85db3a
NataRover <vip.aleksa78@mail.ru>
2023-06-06 04:05:06 +0300

    ---

--- scroll.ts
+++ scroll.ts
@@ -1,22 +1,22 @@
-//Get the button
+//button
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-scrollFunction();
+
+window.onscroll= function () {
+scrollFunction(); 
 };
 
 function scrollFunction() {
 if (
-document.body.scrollTop > 500 ||
-document.documentElement.scrollTop > 500
+document.body.scrollTop > 100 ||
+document.documentElement.scrollTop > 100
 ) {
 mybutton.style.display = "block";
 } else {
 mybutton.style.display = "none";
 }
 }
-// When the user clicks on the button, scroll to the top of the document
+
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
